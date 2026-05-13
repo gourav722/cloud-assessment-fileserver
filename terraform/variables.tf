23abc33dc6f4426ed5dde0c849b510202b01ceb9
Gourav <gourav@example.com>
2026-05-13 09:00:00 +0530

    feat: add Terraform root variables for kind cluster

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "pvc_name" {
+  type = string
+}
+
+variable "storage_size" {
+  type = string
+}
+
+variable "service_port" {
+  type = number
+}
+
+variable "environment" {
+  type = string
+}
